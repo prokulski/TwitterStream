42d85383c583d344e346994880038cca77a9d79f
prokulski <prokulski@gmail.com>
2019-07-22 14:34:45 +0200

    corrected DROP TABLE

--- createDB.py
+++ createDB.py
@@ -3,7 +3,7 @@ import sqlite3
 conn = sqlite3.connect('twitter.db')
 c = conn.cursor()
 
-c.execute('DROP TABLE tweets')
+c.execute('DROP TABLE IF EXISTS tweets')
 conn.commit()
 
 c.execute('''CREATE TABLE tweets
