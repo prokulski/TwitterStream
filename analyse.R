c5aa4f235b0798665f0c110263a0626f174a00d9
prokulski <prokulski@gmail.com>
2019-07-22 14:08:06 +0200

    read data into R

--- analyse.R
+++ analyse.R
@@ -1,10 +1,20 @@
 library(tidyverse)
+library(lubridate)
 library(DBI)
 
+# DB stuff
 conn <- dbConnect(RSQLite::SQLite(), "twitter.db")
 
-dbListTables(conn)
+# load tweets
+tweets <- dbGetQuery(conn, "SELECT * FROM tweets") %>%
+  # correct datetime
+  mutate(datetime = as.POSIXct(strptime(date, "%a %b %d %H:%M:%S %z %Y") %>% with_tz("Europe/Warsaw")) - hours(2)) %>%
+  as_tibble() %>%
+  select(user, tweetText, datetime)
 
-tweets <- dbGetQuery(conn, "SELECT * FROM tweets")
+# close DB
+dbDisconnect(conn)
 
+
+# data table is ready
 tweets
